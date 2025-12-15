3728eb047a8b75936eef57336ce5ab8bc2db6945
Fatima-Khan529 <fatimakhan.zulfiqar@gmail.com>
2025-12-15 17:35:54 +0530

    Add files via upload

---
+++ Week4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week4;
+
+public class Week4 {
+
+    public static void main(String[] args) {
+        int n=19;
+        if(n%2==0)
+            System.out.println(n+" is even");
+        else
+            System.out.println(n+" is odd");
+    }
+    
+}
